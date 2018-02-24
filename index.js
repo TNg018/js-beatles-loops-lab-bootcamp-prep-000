a1f8a9123512dbcf84ffd972e7c27f1f210d9216
Tien Nguyen <pri859@sudomail.com>
2018-02-24 20:41:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function johnLennonFacts() {
 var array = [];
 var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
     while (array.length < facts.length) {
-      array.push("facts" + "!!!");
+      array.push(facts() + "!!!");
     }
 
     return array;
